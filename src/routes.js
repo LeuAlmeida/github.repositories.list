a10101258352a18dc4f18a17eee16f3d01524905
LeuAlmeida <leo@webid.net.br>
2019-12-04 18:59:09 -0300

    Exact path to main page

--- src/routes.js
+++ src/routes.js
@@ -8,7 +8,7 @@ export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" component={Main} />
+        <Route path="/" exact component={Main} />
         <Route path="/repository" component={Repository} />
       </Switch>
     </BrowserRouter>
