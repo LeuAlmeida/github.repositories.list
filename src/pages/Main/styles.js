ba64818fce0f2adc78a3612dfc82f87535ecbe9b
LeuAlmeida <leo@webid.net.br>
2019-12-05 18:52:30 -0300

    Form stylesheet

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -20,6 +20,18 @@ export const Container = styled.div`
   }
 `;
 
-export const Form = styled.form``;
+export const Form = styled.form`
+  margin-top: 30px;
+  display: flex;
+  flex-direction: row;
+
+  input {
+    flex: 1;
+    border: 1px solid #eee;
+    padding: 10px 15px;
+    border-radius: 4px;
+    font-size: 16px;
+  }
+`;
 
 export const SubmitButton = styled.button``;
