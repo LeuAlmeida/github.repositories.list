a3f2e6e25d7716c00b1678a9e4bc43644975f2e6
LeuAlmeida <leo@webid.net.br>
2019-12-05 18:34:42 -0300

    Update background color

--- src/styles/global.js
+++ src/styles/global.js
@@ -13,8 +13,16 @@ export default createGlobalStyle`
  }
 
  body {
-   background: #7159c1;
-   -webkit-font-smoothing: antialiased !important;
+  knob
+  background: rgba(247,101,164,1);
+  background: -moz-linear-gradient(-45deg, rgba(247,101,164,1) 0%, rgba(228,70,71,1) 100%);
+  background: -webkit-gradient(left top, right bottom, color-stop(0%, rgba(247,101,164,1)), color-stop(100%, rgba(228,70,71,1)));
+  background: -webkit-linear-gradient(-45deg, rgba(247,101,164,1) 0%, rgba(228,70,71,1) 100%);
+  background: -o-linear-gradient(-45deg, rgba(247,101,164,1) 0%, rgba(228,70,71,1) 100%);
+  background: -ms-linear-gradient(-45deg, rgba(247,101,164,1) 0%, rgba(228,70,71,1) 100%);
+  background: linear-gradient(135deg, rgba(247,101,164,1) 0%, rgba(228,70,71,1) 100%);
+  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#f765a4', endColorstr='#e44647', GradientType=1 );
+  -webkit-font-smoothing: antialiased !important;
  }
 
  body, input, button {
