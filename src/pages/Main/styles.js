f552e2f619f8af7fddc2bcc9dafdcee3dbf0c6f7
LeuAlmeida <leo@webid.net.br>
2019-12-05 18:56:41 -0300

    Improving submitbutton stylesheet

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -34,4 +34,16 @@ export const Form = styled.form`
   }
 `;
 
-export const SubmitButton = styled.button``;
+export const SubmitButton = styled.button.attrs({
+  type: 'button',
+})`
+  background: #7159c1;
+  border: 0;
+  padding: 0 15px;
+  margin-left: 10px;
+  border-radius: 4px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
