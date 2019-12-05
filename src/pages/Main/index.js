850bbe27c99735f3a7227238c561e8d07b4a6ffc
LeuAlmeida <leo@webid.net.br>
2019-12-05 18:45:23 -0300

    Update index components

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
 
-import { Title } from './styles';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+
+import { Container, Form, SubmitButton } from './styles';
 
 export default function Main() {
   return (
     <Container>
-      <h1>Repositórios</h1>
+      <h1>
+        <FaGithubAlt />
+        Repositórios
+      </h1>
+
+      <Form onSubmit={() => {}}>
+        <input type="text" placeholder="Adicionar repositório" />
+
+        <SubmitButton disabled>
+          <FaPlus color="#FFF" size={14} />
+        </SubmitButton>
+      </Form>
     </Container>
   );
 }
