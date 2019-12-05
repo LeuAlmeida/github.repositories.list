2d931e320929968efaca619ab26cb5467039e5a4
LeuAlmeida <leo@webid.net.br>
2019-12-05 13:01:39 -0300

    Improving main component

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,12 +1,6 @@
 import styled from 'styled-components';
 
 export const Title = styled.h1`
-  font-size: 24px;
-  color: ${props => (props.error ? 'red' : '#7159c1')};
+  color: #fff;
   font-family: Arial, Helvetica, sans-serif;
-
-  small {
-    font-size: 14px;
-    color: #333;
-  }
 `;
