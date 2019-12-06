a182ad5455604122605b4a6c9ac76311a30d516f
LeuAlmeida <leo@webid.net.br>
2019-12-06 14:43:15 -0300

    Include user name

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -72,6 +72,7 @@ export default class Repository extends Component {
                   <a href={issue.html_url}>{issue.title}</a>
                   {/** LABELS */}
                 </strong>
+                <p>{issue.user.login}</p>
               </div>
             </li>
           ))}
