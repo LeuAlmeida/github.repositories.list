5abe35fca1cb1e075ef21a6ecdb00b80b0d89cf6
LeuAlmeida <leo@webid.net.br>
2019-12-06 14:56:25 -0300

    Improving issues stylesheets

--- src/pages/Repository/styles.js
+++ src/pages/Repository/styles.js
@@ -84,6 +84,17 @@ export const IssueList = styled.ul`
             transition: 0.3s;
           }
         }
+
+        span {
+          background: #fff;
+          color: #333;
+          border-radius: 2px;
+          font-size: 12px;
+          font-weight: 600;
+          height: 20px;
+          padding: 3px 4px;
+          margin-left: 10px;
+        }
       }
 
       p {
