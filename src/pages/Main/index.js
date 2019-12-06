cbf02cea36c9bbab8149ed0ea6e25a4d5957ebf8
LeuAlmeida <leo@webid.net.br>
2019-12-06 11:23:01 -0300

    Improving index page

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,26 +1,43 @@
 import React, { Component } from 'react';
-
 import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+
+import api from '../../services/api';
 
 import { Container, Form, SubmitButton } from './styles';
 
 export default class Main extends Component {
   state = {
     newRepo: '',
+    repositories: [],
+    loading: false,
   };
 
   handleInputChange = e => {
     this.setState({ newRepo: e.target.value });
   };
 
-  handleSubmit = e => {
+  handleSubmit = async e => {
     e.preventDefault();
 
-    console.log(this.state.newRepo);
+    this.setState({ loading: true });
+
+    const { newRepo, repositories } = this.state;
+
+    const response = await api.get(`/repos/${newRepo}`);
+
+    const data = {
+      name: response.data.full_name,
+    };
+
+    this.setState({
+      repositories: [...repositories, data],
+      newRepo: '',
+      loading: false,
+    });
   };
 
   render() {
-    const { newRepo } = this.state;
+    const { newRepo, loading } = this.state;
 
     return (
       <Container>
@@ -37,7 +54,7 @@ export default class Main extends Component {
             onChange={this.handleInputChange}
           />
 
-          <SubmitButton disabled>
+          <SubmitButton loading={loading}>
             <FaPlus color="#ec536c" size={14} />
           </SubmitButton>
         </Form>
