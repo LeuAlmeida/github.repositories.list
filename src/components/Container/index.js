bd895ceaa8701809653f3968e391c1acb9700655
LeuAlmeida <leo@webid.net.br>
2019-12-06 14:17:11 -0300

    Improvements and fixes of container component

---
+++ src/components/Container/index.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  max-width: 700px;
+  border-radius: 4px;
+  padding: 30px;
+  margin: 80px auto;
+
+  h1 {
+    font-size: 20px;
+    display: flex;
+    align-items: center;
+    flex-direction: row;
+    color: #fff;
+    font-family: 'Raleway', cursive;
+    text-transform: uppercase;
+  }
+
+  svg {
+    margin-right: 10px;
+  }
+`;
+
+export default Container;
