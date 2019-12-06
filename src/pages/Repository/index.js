3589d3434b0d448af674cab325a19455257d8199
LeuAlmeida <leo@webid.net.br>
2019-12-06 14:11:35 -0300

    Create Loading component

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import api from '../../services/api';
 
-// import { Container } from './styles';
+import { Loading } from './styles';
 
 export default class Repository extends Component {
   static propTypes = {
@@ -43,6 +43,10 @@ export default class Repository extends Component {
 
   render() {
     const { repository, issues, loading } = this.state;
+
+    if (loading) {
+      return <Loading>Carregando</Loading>;
+    }
 
     return <h1>Repository:</h1>;
   }
