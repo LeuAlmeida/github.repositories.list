ac1feef9d9908b0396ba47b63ea27fb7ef55afee
Léu Almeida <leo@webid.net.br>
2019-12-05 23:08:56 -0300

    Fixes and improvements to Main stylesheet

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -2,9 +2,9 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   max-width: 700px;
-  background: #fff;
+  /* background: #ffffff29; */
   border-radius: 4px;
-  box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+  /* box-shadow: 0 0 20px rgba(0, 0, 0, 0.1); */
   padding: 30px;
   margin: 80px auto;
 
@@ -13,6 +13,7 @@ export const Container = styled.div`
     display: flex;
     align-items: center;
     flex-direction: row;
+    color: #fff;
   }
 
   svg {
@@ -27,23 +28,35 @@ export const Form = styled.form`
 
   input {
     flex: 1;
-    border: 1px solid #eee;
+    /* border: 1px solid #eee; */
+    border: 0px;
+    border-bottom: 1px solid #ffffff69;
+    background: transparent;
     padding: 10px 15px;
-    border-radius: 4px;
+    /* border-radius: 4px; */
     font-size: 16px;
+    color: #fff;
+  }
+
+  input::placeholder {
+    color: #ffffffa1;
   }
 `;
 
 export const SubmitButton = styled.button.attrs({
   type: 'button',
 })`
-  background: #7159c1;
+  background: #fff;
   border: 0;
-  padding: 0 15px;
+  padding: 0 14px;
   margin-left: 10px;
-  border-radius: 4px;
+  border-radius: 80px;
 
   display: flex;
   justify-content: center;
   align-items: center;
+
+  svg {
+    margin-right: 0px;
+  }
 `;
