cad68aab0c8d62610137b6ad87077f886ff35ef4
Léu Almeida <leo@webid.net.br>
2019-12-07 16:47:46 -0300

    Create copyrights component

---
+++ src/components/Copyrights/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Footer } from './styles';
+
+export default function Copyrights() {
+  return (
+    <>
+      <Footer>
+        <span>Teste</span>
+      </Footer>
+    </>
+  );
+}
