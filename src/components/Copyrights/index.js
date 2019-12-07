604eec7627032c86fa16096d61e123bfe7b67f11
Léu Almeida <leo@webid.net.br>
2019-12-07 16:54:38 -0300

    Improving copyrights field component

--- src/components/Copyrights/index.js
+++ src/components/Copyrights/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
 
+import { FaHeart, FaMugHot } from 'react-icons/fa';
 import { Footer } from './styles';
 
 export default function Copyrights() {
   return (
     <>
       <Footer>
-        <span>Teste</span>
+        <span>
+          Made by{' '}
+          <a
+            href="https://leunardo.dev"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Léu Almeida
+          </a>{' '}
+          with <FaHeart color="#FFF" size={14} /> and{' '}
+          <FaMugHot color="#FFF" size={14} />
+        </span>
       </Footer>
     </>
   );
